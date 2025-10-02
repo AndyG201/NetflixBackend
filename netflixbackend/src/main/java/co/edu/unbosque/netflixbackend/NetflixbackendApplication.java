58265f4040061209bf5cf121840f1de12d50b62d
AndyG201 <agambac@unbosque.edu.co>
2025-10-02 10:25:53 -0500

    COMMIT

---
+++ netflixbackend/src/main/java/co/edu/unbosque/netflixbackend/NetflixbackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.netflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixbackendApplication.class, args);
+	}
+
+}
